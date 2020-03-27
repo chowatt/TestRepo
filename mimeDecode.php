0c1de1aa105d424514a1571986c15082f63613d1
chowatt <chris@howattech.com>
2020-03-27 12:52:01 -0400

    Update mimeDecode.php

--- mimeDecode.php
+++ mimeDecode.php
@@ -836,7 +836,7 @@ class Mail_mimeDecode extends PEAR
 		 
         $cb = create_function('$matches',  ' return chr(hexdec($matches[0]));');
          
-        $input = preg_replace_callback( '/=([a-f0-9]{2})/i', $cb, $input);
+        $input = preg_replace_callback('/=([a-f0-9]{2})/i', function($m) { return chr(hexdec($m[0])); }, $input);
 
         return $input;
     }
